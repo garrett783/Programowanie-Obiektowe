7dce3b1e5dffc9660dbdb710c399609b052b6313
PW <PW@DESKTOP-8G8IIDP>
2023-10-27 15:45:15 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj temperaturę w stopniach Celcjusza");
+            double C, F;
+            C = Convert.ToDouble(Console.ReadLine());
+            F = 32 + 9.0 / 5 * C;
+            Console.WriteLine(C + " stoppni Celcjusza to " + F + " stopni Fahrenheita");
+            Console.ReadKey();
+        }
+    }
+}
